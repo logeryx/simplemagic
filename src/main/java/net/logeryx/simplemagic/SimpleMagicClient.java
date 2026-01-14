da14139cf4e8169491a1879d1b612c1d109be41f
LogeryX <martin.mensik508@seznam.cz>
2026-01-14 19:58:45 +0100

    New client entrypoint class

---
+++ src/main/java/net/logeryx/simplemagic/SimpleMagicClient.java
@@ -0,0 +1,10 @@
+package net.logeryx.simplemagic;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SimpleMagicClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
